169e64a6bf7e44a5fb5f8339ab2da437fe96e749
yoshikouki <yoshikouki@pepabo.com>
2024-08-11 14:44:34 +0900

    不要な引数

--- src/features/clock/animated-tick.tsx
+++ src/features/clock/animated-tick.tsx
@@ -4,7 +4,7 @@ interface AnimatedClockProps {
   time: Date | null;
 }
 
-export const AnimatedTick: React.FC<AnimatedClockProps> = ({ time }) => {
+export const AnimatedTick: React.FC<AnimatedClockProps> = () => {
   return (
     <div className="absolute inset-0">
       <ClockFace className="animate-clock-tick" />

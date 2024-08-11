610e888c131105dcd9a92bd86bda4ab9558d97f3
yoshikouki <yoshikouki@pepabo.com>
2024-08-11 13:39:58 +0900

    文字サイズを調整

--- src/app/timer.tsx
+++ src/app/timer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { useState } from "react";
 
 type Time = Date | null;
@@ -31,9 +32,11 @@ export const Clock = () => {
   };
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-gradient-to-b from-primary to-pink-500">
       <div
-        className="rounded-lg bg-white/10 p-8 font-bold text-8xl text-white shadow-lg"
+        className={cn(
+          "mx-4 flex w-full items-center justify-center rounded-lg font-black text-[20vw] text-white shadow-lg",
+        )}
         ref={timerRef}
       >
         {formatTime(time)}
